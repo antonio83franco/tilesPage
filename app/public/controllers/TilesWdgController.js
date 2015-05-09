d7cbfe2a45655b7009a05fe97704ad49c884322f
Antonio Franco <antonio@backbase.com>
2015-05-10 00:31:09 +0200

    Adding back swipe listeners

--- app/public/controllers/TilesWdgController.js
+++ app/public/controllers/TilesWdgController.js
@@ -20,14 +20,9 @@ angular.module('tilesApp', [])
     endTile = tilesOnScreen;
 
     _getPagedData();
+    _setupSwipe();
 
-    function _getPagedData (){
-      tilesService.getData(startTile, endTile).then(function(response){
-        $scope.tilesList = response.data;
-        totalItems = response.meta.totalItems;
-      });
-    }
-
+    //Public methods
     $scope.prev = function(){
       startTile--;
       endTile = startTile + tilesOnScreen;
@@ -48,4 +43,51 @@ angular.module('tilesApp', [])
     $scope.showPrev = function() {
       return startTile > 0 ? true : false;
     }
-  });
+
+
+    //Private methods
+    function _getPagedData (){
+      tilesService.getData(startTile, endTile).then(function(response){
+        $scope.tilesList = response.data;
+        totalItems = response.meta.totalItems;
+      });
+    }
+
+    function _setupSwipe() {
+        var xDown = null,
+            yDown = null;
+
+        //Attaching listener to handle swipe
+        document.addEventListener('touchstart', function(evt) {
+            xDown = evt.touches[0].clientX;
+            yDown = evt.touches[0].clientY;
+        },
+        false);
+
+        document.addEventListener('touchmove', function(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* left swipe */
+                    $scope.next();
+                } else {
+                    /* right swipe */
+                    $scope.prev();
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        },
+        false);
+    }
+});
