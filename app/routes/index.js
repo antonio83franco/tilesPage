2db99565e43778d46f3006f4d47a9feeb78d9004
Antonio Franco <antonio@backbase.com>
2015-05-09 16:41:54 +0200

    ejs engine

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { 
+  res.render('index', {
   	placeholder: [{
 		title: "loading data...",
 		price: "1"
